4bfcf44873d53d5780d5252a6f10e7a20978186d
denilsonsrocha <denilsonsrocha@gmail.com>
2019-12-05 10:27:44 -0300

    reorganização das rotas

--- src/server.js
+++ src/server.js
@@ -1,46 +1,47 @@
 require("dotenv").config();
-
 const express = require('express');
-
 const app = express();
-
 const http = require('http').createServer(app);
-
 const io = require('socket.io')(http)
-
 const cors = require('cors');
+
+const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.use(routes);
 
-  res.json({ nome: 'teste' })
+// app.get('/', (req, res) => {
 
-})
+//   res.json({ nome: 'teste' })
 
-io.on('connection', (socket) => {
+// })
 
-  console.log('New connection', socket.id);
+routes.
 
-  //io.sockets.emit('receivedMessage', matriz);
+  io.on('connection', (socket) => {
 
-  socket.on('sendTabuleiro', data => {
-    console.log(data);
+    console.log('New connection', socket.id);
 
-    socket.broadcast.emit('recebeTabuleiro', data);
+    //io.sockets.emit('receivedMessage', matriz);
 
+    socket.on('sendTabuleiro', data => {
+      console.log(data);
+
+      socket.broadcast.emit('recebeTabuleiro', data);
+
+    });
+
+    socket.on('sendJogadorAtual', jogador => {
+
+      console.log(jogador);
+
+      socket.broadcast.emit('recebeJogadorAtual', jogador);
+
+    });
   });
-
-  socket.on('sendJogadorAtual', jogador => {
-
-    console.log(jogador);
-
-    socket.broadcast.emit('recebeJogadorAtual', jogador);
-
-  });
-});
 
 http.listen(process.env.PORT || 3333, function () {
 
